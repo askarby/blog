6da89dfcaba949bf2e2e8affae4b3c9af0ceea95
askarby <abs@inno-tech.dk>
2021-05-04 22:41:06 +0200

    feat: added RxJs operator to map an ScullyRoute into a Post

--- src/app/blog/operators/map-to-post.operator.spec.ts
+++ src/app/blog/operators/map-to-post.operator.spec.ts
@@ -2,13 +2,23 @@ import { ScullyRoute } from '@scullyio/ng-lib';
 import { createPostAsScullyRoute } from '../../testing/post.test-utils';
 import { of } from 'rxjs';
 import { mapToPost } from './map-to-post.operator';
+import { dateOfYear } from '../../testing/date.test-utils';
 
 describe('mapToPost operator', () => {
   let route: ScullyRoute;
+  let publishedAt: Date;
 
   beforeEach(() => {
+    publishedAt = dateOfYear(2021)
+      .withMonth('apr')
+      .withDay(25)
+      .withTime('12:34:56')
+      .build();
+
     route = createPostAsScullyRoute({
       tags: ['a', 'b', 'c'],
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      published_date: publishedAt,
     });
   });
 
@@ -17,6 +27,15 @@ describe('mapToPost operator', () => {
       .pipe(mapToPost())
       .subscribe((post) => {
         expect(post.tags).toEqual(['a', 'b', 'c']);
+        done();
+      });
+  });
+
+  it('should map published_date to an Date', (done) => {
+    of(route)
+      .pipe(mapToPost())
+      .subscribe((post) => {
+        expect(post.published_date).toEqual(publishedAt);
         done();
       });
   });
--- src/app/blog/operators/map-to-post.operator.ts
+++ src/app/blog/operators/map-to-post.operator.ts
@@ -3,19 +3,25 @@ import { ScullyRoute } from '@scullyio/ng-lib';
 import { Observable, OperatorFunction } from 'rxjs';
 import { map } from 'rxjs/operators';
 
+const routeToPost = (route: ScullyRoute): Post => {
+  const post = {} as Post;
+  post.tags = [];
+  Object.entries(route).forEach(([key, value]) => {
+    if (key === 'tags') {
+      post.tags = value.split(' ');
+    } else if (key === 'published_date') {
+      post.published_date = new Date(value);
+    } else {
+      post[key] = value;
+    }
+  });
+  return post;
+};
+
 export const mapToPost = (): OperatorFunction<ScullyRoute, Post> => (
   input: Observable<ScullyRoute>
-) =>
-  input.pipe(
-    map((route) => {
-      const post = {} as Post;
-      Object.entries(route).forEach(([key, value]) => {
-        if (key === 'tags') {
-          post.tags = value.split(' ');
-        } else {
-          post[key] = value;
-        }
-      });
-      return post;
-    })
-  );
+) => input.pipe(map(routeToPost));
+
+export const mapToPosts = (): OperatorFunction<ScullyRoute[], Post[]> => (
+  input: Observable<ScullyRoute[]>
+) => input.pipe(map((routes) => routes.map(routeToPost)));
